e4a913fd327477aff208dc01f857afecfbf9da7f
cocoJambo <111556231+VbotR@users.noreply.github.com>
2025-03-05 00:51:55 +0300

    Update AppShell.xaml.cs

--- MauiApp1/AppShell.xaml.cs
+++ MauiApp1/AppShell.xaml.cs
@@ -5,11 +5,6 @@
         public AppShell()
         {
             InitializeComponent();
-
-            // Регистрируем маршруты для навигации
-            Routing.RegisterRoute(nameof(BadmintonPage), typeof(BadmintonPage));
-            Routing.RegisterRoute(nameof(RunPage), typeof(RunPage));
-            Routing.RegisterRoute(nameof(BasketballPage), typeof(BasketballPage));
         }
     }
 }
